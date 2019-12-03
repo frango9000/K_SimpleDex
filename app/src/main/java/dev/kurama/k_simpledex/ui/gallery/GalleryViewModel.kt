1a8297a7c733914b7f1e2db675e0573211e90912
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-12-03 16:56:38 +0100

    init android navigation drawer

---
+++ app/src/main/java/dev/kurama/k_simpledex/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package dev.kurama.k_simpledex.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}

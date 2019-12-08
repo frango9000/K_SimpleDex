8e0ead9cd0821f5ea464ae7a0b644015b2dba5d9
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-12-08 20:06:43 +0100

    0.0.1-PoC screenshots variable max pokemon to load

--- app/src/main/java/dev/kurama/k_simpledex/SplashActivity.kt
+++ app/src/main/java/dev/kurama/k_simpledex/SplashActivity.kt
@@ -9,17 +9,20 @@ import java.lang.ref.WeakReference
 
 class SplashActivity : AppCompatActivity() {
 
-    val maxPokeToLoad: Int = R.integer.max_pokemon_to_load
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val initTask = InitPokemonTask(WeakReference(this)).execute(resources.getInteger(maxPokeToLoad))
+        val initTask = InitPokemonTask(WeakReference(this)).execute(resources.getInteger(R.integer.max_pokemon_to_load))
     }
 
     fun progressBarUpdate(status: Int) {
         progressBar.setProgress(status, true)
-        textProgress.text = String.format(getString(R.string.progres_text_format), status)
+        textProgress.text = String.format(
+            getString(R.string.progres_text_format),
+            status,
+            resources.getInteger(R.integer.max_pokemon_to_load)
+        )
     }
 
     fun notifyDoneFetching(size: Int) {
